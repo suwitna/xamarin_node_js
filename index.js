6b5bfdda5c763c3dcad609b3f6aa25c28e5baf17
suwitna <44050482+suwitna@users.noreply.github.com>
2020-04-18 23:43:23 +0700

    update code for webhook

--- index.js
+++ index.js
@@ -547,7 +547,7 @@ app.post('/webhook',(req,res,next)=> {
     console.log(`Message token : ${ reply_token }`);
     console.log(`Message user Type : ${ user_type }`);
     console.log(`Message user ID : ${ user_id }`);
-    console.log(`Message user ID : ${ msg }`);
+    console.log(`Message : ${ msg }`);
     //reply(reply_token);
     res.sendStatus(200);
     //var replyToken = req.body.events[0].replyToken;
