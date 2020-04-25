07b1784158ddca3c3ec73a025c57bbf964fabd23
suwitna <44050482+suwitna@users.noreply.github.com>
2020-04-26 01:35:41 +0700

    update package.json

--- index.js
+++ index.js
@@ -266,6 +266,42 @@ app.post('/DenOfArtUserExist',(req,res,next)=> {
     });
 });
 
+app.post('/DenOfArtFindUser',(req,res,next)=> { 
+    console.log("HTTP POST Request :: Den of Art Check Find User");
+    var post_data = req.body;  
+  
+    var loginname = post_data.username;
+
+    // Get a reference to the database service
+    var db = firebase.database();
+    var dbRef = db.ref('DenOfArtUser');
+    var existUser = false;
+
+    dbRef.orderByChild('UserName').equalTo(loginname).once('value', (snapshot)=>{
+        var vals = snapshot.val();
+        console.log('vals:', vals);
+        if(vals != null && vals != ''){
+            var keys = Object.keys(vals);
+            var obj = {};
+            console.log('keys.length:', keys.length);
+            for(var i=0; i<keys.length; i++){
+                var k = keys[i];
+                var user = vals[k].UserName;
+                
+                if(loginname == user){
+                    obj[k] = vals[k];
+                    res.json(vals[k]);
+                    return;
+                }
+            }
+        }
+
+        console.log('User not found');
+        res.send('User not found');
+        return;
+    });
+});
+
 app.post('/DenOfArtGetUserData',(req,res,next)=> { 
     console.log("HTTP POST Request :: Den of Art Get User Data");
     var post_data = req.body;  
